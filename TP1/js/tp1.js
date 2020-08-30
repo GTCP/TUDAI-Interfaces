4d0c3939ed6b7e4f5fbb986b2bfc01d75c23d63c
GTCP <55481093+GTCP@users.noreply.github.com>
2020-08-30 04:42:28 -0300

    Revert "Tp1 Ejercicio 5 Terminado"
    This reverts commit 5149a2f45832425eb6d6b98d756622f1b4b8ac65.

--- TP1/js/tp1.js
+++ TP1/js/tp1.js
@@ -3,9 +3,8 @@ ejercicio_1();
 ejercicio_2();
 ejercicio_3();
 ejercicio_4();
-ejercicio_5();
 function ejercicio_1(){  
-    let matriz=[];
+    let matriz = [];
     let rows=12;
     let cols=12;
     inicializar_matriz(rows,cols,matriz);
@@ -13,18 +12,21 @@ function ejercicio_1(){
     let valorMaximoDeTodaLaMatriz=valor_maximo_de_toda_la_matriz(rows,cols,matriz);
     let valorMaximoPorFilaPares=valor_maximo_por_fila_pares(rows,cols,matriz);
     let valorMinimoPorFilaImpares=valor_minimo_por_fila_Impares(rows,cols,matriz);
+    //let resultadoA = document.getElementById("resultadoA");
+    //let resultadoB1 = document.getElementById("resultadoB1");
+   // let resultadoB2 = document.getElementById("resultadoB2");
     imprimir_promedio(rows,cols,matriz);
-    resultadoA.innerHTML="El mayor de toda la matriz es: "+valorMaximoDeTodaLaMatriz;
-    resultadoB1.innerHTML="El valor maximo de las filas pares es: "+valorMaximoPorFilaPares;
-    resultadoB2.innerHTML="El valor minimo de filas impares es :"+valorMinimoPorFilaImpares;   
+    resultadoA.innerHTML = "El mayor de toda la matriz es: "+valorMaximoDeTodaLaMatriz;
+    resultadoB1.innerHTML = "El valor maximo de las filas pares es: "+valorMaximoPorFilaPares;
+    resultadoB2.innerHTML = "El valor minimo de filas impares es :"+valorMinimoPorFilaImpares;   
     function imprimir_matriz(rows,cols,matriz){
-        let tBody=document.getElementById("tBodyMatriz");
-            for (let x=0;x<rows;x++) {
-                let row=document.createElement("tr");
+        let tBody = document.getElementById("tCuerpo");//trae del DOM el tCuerpo
+            for (let x = 0; x < rows; x++) {
+                let row = document.createElement("tr");
                 let col;
-                for (let y=0;y<cols;y++) {
-                    col=document.createElement("td");
-                    let datoCol=document.createTextNode(matriz[x][y]);
+                for (let y = 0; y < cols; y++) {
+                    col = document.createElement("td");
+                    let datoCol = document.createTextNode(matriz[x][y]);
                     col.appendChild(datoCol);
                     row.appendChild(col);
                 }
@@ -38,10 +40,10 @@ function ejercicio_1(){
              }
     }
     function inicializar_matriz(rows,cols,matriz){
-        for(let x=0;x<rows;x++){
-            matriz[x]=[];
-            for(let y=0;y<cols;y++){
-                matriz[x][y]=(parseInt)(Math.random()*100);
+        for(let x =0;x< rows; x++){
+            matriz[x] = [];
+            for(let y=0; y < cols ; y++){
+                matriz[x][y]= (parseInt)(Math.random()*100);
                 console.log("["+matriz[x][y]+"]");
             }
         }
@@ -49,8 +51,8 @@ function ejercicio_1(){
     }  
     function valor_maximo_de_toda_la_matriz(rows,cols,matriz){
         let valorMaximo=0;
-        for(let x=0;x<rows;x++){
-            for(let y=0;y<cols;y++){
+        for(let x =0;x< rows; x++){
+            for(let y=0; y < cols ; y++){
                 if(matriz[x][y]>valorMaximo){
                     valorMaximo=matriz[x][y];
                 }
@@ -62,8 +64,8 @@ function ejercicio_1(){
     }
     function valor_maximo_por_fila_pares(rows,cols,matriz){
         let valorMaximo=0;
-        for(let x=0;x<rows;x++){
-            for(let y=0;y<cols;y++){
+        for(let x =0;x< rows; x++){
+            for(let y=0; y < cols ; y++){
                 if(x%2==0){
                     if(matriz[x][y]>valorMaximo){
                         valorMaximo=matriz[x][y];
@@ -77,8 +79,8 @@ function ejercicio_1(){
     }
     function valor_minimo_por_fila_Impares(rows,cols,matriz){
         let valorMinimo=99;
-        for(let x=0;x<rows;x++){
-            for(let y=0;y<cols;y++){
+        for(let x =0;x< rows; x++){
+            for(let y=0; y < cols ; y++){
                 if(x%2!=0){
                     if(matriz[x][y]<valorMinimo){
                         valorMinimo=matriz[x][y];
@@ -92,48 +94,56 @@ function ejercicio_1(){
     }
     function promedio_de_cada_fila_en_arreglo(rows,cols,matriz){
         let promedioPorFila=0;
-        let promedio=[];
-            for(let x=0;x<rows;x++){
-                for(let y=0;y<cols;y++){
+        let promedio = [];
+            for(let x =0;x< rows; x++){
+                for(let y=0; y < cols ; y++){
                     promedioPorFila+=matriz[x][y];
                 }
                 promedioPorFila=promedioPorFila/cols;
                 promedio[x]=promedioPorFila;
                 promedioPorFila=0;
             }
+            //Ver arreglo
+           // for(let x=0;x<promedio.length;x++){
+           //     console.log(promedio[x]);
+           // }
+    
         console.log("promedio_de_cada_fila_en_arreglo OFF");
         return promedio;
     }
     console.log("Ejercicio1 OFF");
 }
 function ejercicio_2(){
-        let c=document.getElementById("canvas-ejercicio2");
-        let con=c.getContext("2d");
-        let width=c.width;
-        let height=c.height;  
-        con.fillStyle="rgb(255,0,0)";
-        con.fillRect(0,0,width,height);  
+        let c = document.getElementById("canvas-ejercicio2");
+        let con = c.getContext("2d");
+        let width= c.width;
+        let height= c.height;  
+        con.fillStyle = "rgb(255,0,0)";
+        con.fillRect(0, 0, width, height);  
+
         console.log("Ejercicio2 OFF");
+       
 }
 function ejercicio_3(){
-    let c=document.getElementById("canvas-ejercicio3");
-    let con=c.getContext("2d");
-    let height=c.height;
-    let width=c.width;
-    let imageData=con.createImageData(c.width,c.height);
-    set_pixeles(imageData,0,0,255,255);
+
+    let c = document.getElementById("canvas-ejercicio3");
+    let con = c.getContext("2d");
+    let height = c.height;
+    let width = c.width;
+    let imageData = con.createImageData(c.width, c.height);
+    set_pixeles(imageData, 0, 0, 255, 255);
     
-    function set_pixeles(image,r,g,b,a){
-        for (let x=0;x<width;x++) {
-            for (let y=0;y<height;y++) {
-                let index=(x+y*width)*4;
-                image.data[index+0]=r;
-                image.data[index+1]=g;
-                image.data[index+2]=b;
-                image.data[index+3]=a; 
+    function set_pixeles(image, r, g, b, a){
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                let index = (x+y*width)*4;
+                image.data[index+0] = r;
+                image.data[index+1] = g;
+                image.data[index+2] = b;
+                image.data[index+3] = a; 
             }
         }
-        con.putImageData(imageData,0,0);
+        con.putImageData(imageData, 0, 0 );
     }   
     console.log("Ejercicio3 OFF");
 }
@@ -142,7 +152,7 @@ function ejercicio_4(){
     let con=c.getContext("2d");
     let height=c.height;
     let width=c.width;
-    let imageData=con.createImageData(c.width,c.height);
+    let imageData = con.createImageData(c.width,c.height);
     for(let x=0;x<width;x++){
         for(let y=0;y<height;y++){
             let color=(y/height)*255;
@@ -152,50 +162,10 @@ function ejercicio_4(){
     con.putImageData(imageData,0,0);
     function set_pixeles(image,x,y,color){
         let index=(x+y*image.width)*4;
-        image.data[index+0]=color;
-        image.data[index+1]=color;
-        image.data[index+2]=color;
-        image.data[index+3]=255;
+        image.data[index+0]= color;
+        image.data[index+1]= color;
+        image.data[index+2]= color;
+        image.data[index+3]= 255;
     }
     console.log("Ejercicio4 OFF");
 }
-function ejercicio_5(){
-    let c=document.getElementById("canvas-ejercicio5");
-    let con=c.getContext("2d");
-    let imageData=con.createImageData(c.width,c.height*2);
-    let height=imageData.height;
-    let width=imageData.width;
-    let mitadAnchoTotal=width/2;
-    let r=0;
-    let g=0;
-    let b=0;
-    primera_mitad();
-    segunda_mitad();
-    function primera_mitad(){ 
-        for(let x=0;x<=mitadAnchoTotal;x++){
-            for(let y=0;y<=height;y++){
-                r=(x/mitadAnchoTotal)*255;
-                g=(x/mitadAnchoTotal)*255;
-                set_pixeles(imageData,x,y,r,g,b,255);
-            }
-        }
-    }
-    function segunda_mitad(){
-        for(let x=mitadAnchoTotal;x<width;x++){ 
-            g=(1-((x-mitadAnchoTotal)/mitadAnchoTotal))*255;
-            for(let y=0;y<height;y++){
-                set_pixeles(imageData,x,y,r,g,0,255);
-            }
-        }
-        con.putImageData(imageData,0,0);
-    }
-    function set_pixeles(imageData,x,y,r,g,b,a){
-        let index=(x+y*imageData.width)*4;
-        imageData.data[index+0]=r;
-        imageData.data[index+1]=g;
-        imageData.data[index+2]=b;
-        imageData.data[index+3]=a;
-    }
-    console.log("Ejercicio5 OFF");
-}
-
