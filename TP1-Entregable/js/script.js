46e6b906b69aecab96fb9d5965f8843b53c9a2b9
GTCP <55481093+GTCP@users.noreply.github.com>
2020-09-15 00:01:03 -0300

    Filtros no se pisan, lapiz y goma no se desconfigura
    mejora visual

--- TP1-Entregable/js/script.js
+++ TP1-Entregable/js/script.js
@@ -2,12 +2,18 @@
 Entregable();
 function Entregable(){
     let c=document.querySelector(".canvas-ejercicio");
+    let image = new Image();
     let con=c.getContext('2d');
     let img=new Image();
     let imageData;
+    let imageDataOriginal;
     let imagen= document.getElementById("download");
     img.crossOrigin="Anonymous";
-    img.src="http://4.bp.blogspot.com/-1QdTjGJEBWc/T3s--K-hskI/AAAAAAAAC-w/uk27W0ypb2c/s1600/EL-UNIVERSO-061.jpg";
+    let botonBorradoAutomatico=document.getElementById("borrado-automatico").addEventListener("click",function(){       
+        con.clearRect(0, 0, c.width, c.height);
+    });
+  
+   // img.src="http://4.bp.blogspot.com/-1QdTjGJEBWc/T3s--K-hskI/AAAAAAAAC-w/uk27W0ypb2c/s1600/EL-UNIVERSO-061.jpg";
 
     img.onload=function(){
         myDrawImage(this);
@@ -19,7 +25,37 @@ function Entregable(){
             c.height=img.naturalHeight;
             con.drawImage(img,0,0);
     }
- 
+ let botonSepia=document.getElementById("filtro-sepia").addEventListener("click",function(){
+        filtro_sepia(imageData);
+    });
+    function filtro_sepia(imageData){
+        for (let x=0;x<c.width; x++){
+           for(let y=0;y<c.height;y++){
+               let r=rojo(imageData,x,y);
+               let g=verde(imageData,x,y);
+               let b=azul(imageData,x,y);
+               let f=0.2126*r+0.7152*g+0.0722*b;
+               set_pixeles(imageData,x,y,r,g,b);
+           }
+       }
+       function rojo(imageData,x,y){
+           return imageData.data[(x+y*imageData.width)*4+0];
+       }
+       function verde(imageData,x,y){
+           return imageData.data[(x+y*imageData.width)*4+1];
+       }
+       function azul(imageData,x,y){
+           return imageData.data[(x+y*imageData.width)*4+2];
+       }
+     
+       function set_pixeles(imageData,x,y,r,g,b){
+           let index=(x+y*imageData.width)*4;
+           imageData.data[index+0]=( r * .393 ) + ( g *.769 ) + ( b * .189 );
+           imageData.data[index+1]=( r * .349 ) + ( g *.686 ) + ( b * .168 );
+           imageData.data[index+2]=( r * .272 ) + ( g *.534 ) + ( b * .131 );
+       }
+       con.putImageData(imageData,0,0);
+} 
 //-------------------------- DESCARGAR ARCHIVO----------------------------------------------------------------------------------------------------
     let botonDescargarImagen=document.getElementById("download").addEventListener("click",function(){       
             descargar_imagen();   
@@ -47,30 +83,163 @@ function Entregable(){
             let proceso = new FileReader();
             proceso.readAsDataURL(file);
             proceso.onloadend = function (e) {
-                let image = new Image();
+                
                 image.src = e.target.result;
+                
                 image.onload = function (ev) {
                     let canvas = document.getElementById("canvas-ejercicio");
-                    canvas.width = image.width;
-                    canvas.height = image.height;
-                    let con = canvas.getContext("2d");
+               //     canvas.width = image.width;
+                //    canvas.height = image.height;
+                cargar_image();
+                function cargar_image(){
+                    image.width=canvas.width;
+                    image.height=canvas.height;
+                    imageDataOriginal=image;
+
+                    
                     con.drawImage(image, 0, 0, canvas.width, canvas.height); 
-                };
-            };
+                    imageData=con.getImageData(0,0,canvas.width,canvas.height);
+                    con.putImageData(imageData,0,0);
+
+                }
+                 };
+                 var color= "black";
+var grosor= 2;
+// let activarLapiz=false;
+// let activarGoma=false;
+
+let dibujando = false;
+let borrando = false;
+
+
+var canvas=document.getElementById('canvas-ejercicio');
+var ctx=canvas.getContext('2d');
+let botonlapiz=document.getElementById('lapiz');
+let goma=document.getElementById('goma');
+
+
+ botonlapiz.addEventListener('click', function(e){
+        borrando=false;
+        let rect= canvas.getBoundingClientRect();
+        let x=0;
+        let y=0;
+        dibujando= false;
+        let color= "black";
+        let grosor= 2;
+        let inputColor=document.getElementById('color');
+     
+    
+        canvas.addEventListener('mousedown', function(e){
+            x=e.clientX - rect.left;
+            y=e.clientY - rect.top;
+            dibujando=true;
+            borrando=false;
+        });
+        canvas.addEventListener('mousemove', function(e){
+            if(dibujando===true){
+                dibujar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+                x=e.clientX - rect.left;
+                y=e.clientY - rect.top;
+            }
+        });
+        canvas.addEventListener('mouseup', function(e){
+            if(dibujando===true){
+                dibujar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+                x=0;
+                y=0;
+                dibujando=false;
+            }
+        });
+        function dibujar(x1,y1,x2,y2){
+            ctx.strokeStyle=color;
+            ctx.lineWidth=grosor;
+            ctx.lineCap = "round";
+            ctx.beginPath();
+            ctx.moveTo(x1,y1);
+            ctx.lineTo(x2,y2);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    
+});
+
+goma.addEventListener('click', function(e){
+    
+   
+    var rect= canvas.getBoundingClientRect();
+    var x=0;
+    var y=0;
+    borrando= false;
+    var grosor= 10;
+
+
+    
+
+    function defgrosor(g){
+        grosor=g;
+    }
+    canvas.addEventListener('mousedown', function(e){
+        x=e.clientX - rect.left;
+        y=e.clientY - rect.top;
+        borrando=true;
+    });
+
+    canvas.addEventListener('mousemove', function(e){
+        if(borrando===true){
+            borrar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+            x=e.clientX - rect.left;
+            y=e.clientY - rect.top;
         }
     });
+
+    canvas.addEventListener('mouseup', function(e){
+        if(borrando===true){
+            borrar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+            x=0;
+            y=0;
+            borrando=false;
+        }
+    });
+
+    function borrar(x1, y1, x2,y2){
+        ctx.strokeStyle="white";
+        ctx.lineWidth=grosor;
+        ctx.beginPath();
+        ctx.moveTo(x1,y1);
+        ctx.lineTo(x2,y2);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+});
+
+            };
+        
+    
 
 
 
 //-------------------------- FILTRO CONTRASTE----------------------------------------------------------------------------------------------------
     let botonFiltroContraste=document.getElementById("filtro-contraste").addEventListener("click",function(){       
-        filtro_contraste(imageData);  
+        filtro_contraste();  
     });
     function filtro_contraste(){
+
+
+                    image.width=canvas.width;
+                    image.height=canvas.height;
+                    imageDataOriginal=image;
+
+                    
+                    con.drawImage(image, 0, 0, canvas.width, canvas.height); 
+                    imageData=con.getImageData(0,0,canvas.width,canvas.height);
+                    con.putImageData(imageData,0,0);;
+
+                    
         let C=100;
         let FACTOR = ( 259*( C + 255 ) ) / ( 255*( 259 - C ) );
     
-    
+        
         for (let x=0;x<c.width; x++){
             for(let y=0;y<c.height;y++){
                 let r=rojo(imageData,x,y);
@@ -102,7 +271,9 @@ function Entregable(){
   
 //-------------------------- BORRADO TOTAL----------------------------------------------------------------------------------------------------
     let botonBorradoAutomatico=document.getElementById("borrado-automatico").addEventListener("click",function(){       
-        borrado_automatico(imageData);
+        con.clearRect(0, 0, c.width, c.height);
+
+        borrado_automatico();
     }); 
     function borrado_automatico(){
         for (let x=0;x<c.width; x++){
@@ -135,8 +306,15 @@ function Entregable(){
     let botonNegativo=document.getElementById("filtro-negativo").addEventListener("click",function(){
         filtro_negativo(imageData);
     });
-    function filtro_negativo(imageData){
-        
+    function filtro_negativo(){
+        image.width=canvas.width;
+                    image.height=canvas.height;
+                    imageDataOriginal=image;
+
+                    
+                    con.drawImage(image, 0, 0, canvas.width, canvas.height); 
+                    imageData=con.getImageData(0,0,canvas.width,canvas.height);
+                    con.putImageData(imageData,0,0);;
         for (let x=0;x<c.width; x++){
             for(let y=0;y<c.height;y++){
                 let r=rojo(imageData,x,y);
@@ -168,7 +346,43 @@ function Entregable(){
     let botonGris=document.getElementById("filtro-gris").addEventListener("click",function(){
         filtro_gris(imageData);
     });
-    function filtro_gris(imageData){
+    function filtro_gris(){
+        borrado_automatico();
+        function borrado_automatico(){
+            for (let x=0;x<c.width; x++){
+                for(let y=0;y<c.height;y++){
+                    let r=rojo(imageData,x,y);
+                    let g=verde(imageData,x,y);
+                    let b=azul(imageData,x,y);
+                    let f=255;
+                    set_pixeles(imageData,x,y,r,g,b);
+                }
+            }
+            function rojo(imageData,x,y){
+                return imageData.data[(x+y*imageData.width)*4+0];
+            }
+            function verde(imageData,x,y){
+                return imageData.data[(x+y*imageData.width)*4+1];
+            }
+            function azul(imageData,x,y){
+                return imageData.data[(x+y*imageData.width)*4+2];
+            }
+            function set_pixeles(imageData,x,y,r,g,b){
+                let index=(x+y*imageData.width)*4;
+                imageData.data[index+0]=  255;
+                imageData.data[index+1]=  255;
+                imageData.data[index+2]=  255;
+            }
+            con.putImageData(imageData,0,0);
+        }
+        image.width=canvas.width;
+                    image.height=canvas.height;
+                    imageDataOriginal=image;
+
+                    
+                    con.drawImage(image, 0, 0, canvas.width, canvas.height); 
+                    imageData=con.getImageData(0,0,canvas.width,canvas.height);
+                    con.putImageData(imageData,0,0);;
         for (let x=0;x<c.width; x++){
             for(let y=0;y<c.height;y++){
                 let r=rojo(imageData,x,y);
@@ -200,6 +414,16 @@ function Entregable(){
         filtro_sepia(imageData);
     });
     function filtro_sepia(){
+        image.width=canvas.width;
+                    image.height=canvas.height;
+                    imageDataOriginal=image;
+
+                    
+                    con.drawImage(image, 0, 0, canvas.width, canvas.height); 
+                    imageData=con.getImageData(0,0,canvas.width,canvas.height);
+                    con.putImageData(imageData,0,0);;
+        
+      
         for (let x=0;x<c.width; x++){
            for(let y=0;y<c.height;y++){
                let r=rojo(imageData,x,y);
@@ -227,6 +451,6 @@ function Entregable(){
        }
        con.putImageData(imageData,0,0);
 }  
-
-
+}
+});
 }
