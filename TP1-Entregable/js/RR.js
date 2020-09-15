5dbbc0f27a3e76289f79bb72b669a29cdeae8f3c
GTCP <55481093+GTCP@users.noreply.github.com>
2020-09-15 08:20:45 -0300

    Resubida error de push
    descripcion anterior,  creacion filtro blur, arreglo codigo js.

--- TP1-Entregable/js/RR.js
+++ TP1-Entregable/js/RR.js
@@ -1,52 +1,42 @@
-var color= "black";
-var grosor= 2;
-// let activarLapiz=false;
-// let activarGoma=false;
-
+let color= "black";
+let grosor= 2;
 let dibujando = false;
 let borrando = false;
-
-
-var canvas=document.getElementById('canvas-ejercicio');
-var ctx=canvas.getContext('2d');
-let botonlapiz=document.getElementById('lapiz');
-let goma=document.getElementById('goma');
-
-let inputColor=document.getElementById('color');
-canvas.width = 500;
-canvas.height = 500;
- botonlapiz.addEventListener('click', function(e){
-
-  
-            borrando=false;
+let canvas=document.getElementById("canvas-ejercicio");
+let ctx=canvas.getContext("2d");
+let botonlapiz=document.getElementById("lapiz");
+let goma=document.getElementById("goma");
+let inputColor=document.getElementById("color");
+canvas.width=500;
+canvas.height=500;
+botonlapiz.addEventListener("click",function(e){
+        borrando=false;
         let rect= canvas.getBoundingClientRect();
         let x=0;
         let y=0;
-        dibujando= false;
-        let grosor= 2;
+        dibujando=false;
+        let grosor=2;
         let color="black";
-       
         function defcolor(g){
              let color=g;
              return color;
         }
-    
-        canvas.addEventListener('mousedown', function(e){
-            x=e.clientX - rect.left;
-            y=e.clientY - rect.top;
+        canvas.addEventListener("mousedown",function(e){
+            x=e.clientX-rect.left;
+            y=e.clientY-rect.top;
             dibujando=true;
             borrando=false;
         });
-        canvas.addEventListener('mousemove', function(e){
+        canvas.addEventListener("mousemove",function(e){
             if(dibujando===true){
-                dibujar(x,y,e.clientX - rect.left,e.clientY-rect.top);
-                x=e.clientX - rect.left;
-                y=e.clientY - rect.top;
+                dibujar(x,y,e.clientX-rect.left,e.clientY-rect.top);
+                x=e.clientX-rect.left;
+                y=e.clientY-rect.top;
             }
         });
-        canvas.addEventListener('mouseup', function(e){
+        canvas.addEventListener("mouseup",function(e){
             if(dibujando===true){
-                dibujar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+                dibujar(x,y,e.clientX-rect.left,e.clientY-rect.top);
                 x=0;
                 y=0;
                 dibujando=false;
@@ -55,54 +45,45 @@ canvas.height = 500;
         function dibujar(x1,y1,x2,y2){
             ctx.strokeStyle=color;
             ctx.lineWidth=grosor;
-            ctx.lineCap = "round";
+            ctx.lineCap="round";
             ctx.beginPath();
             ctx.moveTo(x1,y1);
             ctx.lineTo(x2,y2);
             ctx.stroke();
             ctx.closePath();
         }
-    
 });
-
-goma.addEventListener('click', function(e){
-    
-   
-    var rect= canvas.getBoundingClientRect();
-    var x=0;
-    var y=0;
+goma.addEventListener("click",function(e){ 
+    let rect= canvas.getBoundingClientRect();
+    let x=0;
+    let y=0;
     borrando= false;
-    var grosor= 10;
+    let grosor= 10;
   
-    
-
     function defgrosor(g){
         grosor=g;
     }
-    canvas.addEventListener('mousedown', function(e){
-        x=e.clientX - rect.left;
-        y=e.clientY - rect.top;
+    canvas.addEventListener("mousedown",function(e){
+        x=e.clientX-rect.left;
+        y=e.clientY-rect.top;
         borrando=true;
     });
-
-    canvas.addEventListener('mousemove', function(e){
+    canvas.addEventListener("mousemove", function(e){
         if(borrando===true){
-            borrar(x,y,e.clientX - rect.left,e.clientY-rect.top);
-            x=e.clientX - rect.left;
-            y=e.clientY - rect.top;
+            borrar(x,y,e.clientX-rect.left,e.clientY-rect.top);
+            x=e.clientX-rect.left;
+            y=e.clientY-rect.top;
         }
     });
-
-    canvas.addEventListener('mouseup', function(e){
+    canvas.addEventListener("mouseup",function(e){
         if(borrando===true){
-            borrar(x,y,e.clientX - rect.left,e.clientY-rect.top);
+            borrar(x,y,e.clientX-rect.left,e.clientY-rect.top);
             x=0;
             y=0;
             borrando=false;
         }
     });
-
-    function borrar(x1, y1, x2,y2){
+    function borrar(x1,y1,x2,y2){
         ctx.strokeStyle="white";
         ctx.lineWidth=grosor;
         ctx.beginPath();
@@ -111,7 +92,4 @@ goma.addEventListener('click', function(e){
         ctx.stroke();
         ctx.closePath();
     }
-
 });
-
-
